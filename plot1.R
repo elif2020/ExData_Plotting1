88d614a4b53af5544fff76d9cae8a542fd578f28
Elif U <elifcs2020@gmail.com>
2021-04-03 01:16:30 -0400

    initial commit plot1 script

---
+++ plot1.R
@@ -0,0 +1,50 @@
+library(data.table)
+#open connection to the data file
+con<-file("household_power_consumption.txt","r")
+
+#open a data frame with the same column names
+varnames<-scan(con,what="",nlines=1,sep=";",quote="\"",)
+powerdata<-data.frame(matrix(ncol=length(varnames),nrow=0))
+colnames(powerdata)<-varnames
+
+#set a dummy variable to exit after 2/2/2007
+dummy<-FALSE
+
+#read necessary lines
+while(TRUE){
+  line<-readLines(con,1)
+  if(length(line)==0){
+    break
+  }
+  else if(grepl("^1/2/2007|^2/2/2007",line)){
+    line<-as.data.frame(strsplit(line,";"))
+    line<-transpose(line)
+    colnames(line)<-varnames
+    powerdata<-rbind(powerdata,line) 
+    dummy<-TRUE
+  }
+  else if(dummy==TRUE){
+    break
+  }
+}
+
+#close connection
+close(con)
+
+#paste Date and Time together
+powerdata$Time<-do.call(paste,powerdata[,1:2])
+
+#change class of Date/Time into POSIXlt
+powerdata$Time<-strptime(powerdata$Time,format="%d/%m/%Y %H:%M:%OS")
+
+#change the classes of other variables to numeric
+powerdata[,3:length(varnames)]<-lapply(powerdata[,3:length(varnames)],
+                                       as.numeric) 
+
+#plot histogram of Global active power                                                                               as.numeric) 
+png("plot1.png")
+hist(powerdata$Global_active_power,main="Global Active Power",
+                    xlab="Global Active Power(kilowatts)",col="red")
+
+#close the graphics device 
+dev.off()
